cad512432b4f7387c711c49aebbb0c147128f04c
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-03 11:55:22 +0200

    delete

--- my_test_module/models/test_model.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class TestModel(models.Model):
-    _name = 'my_test_module.test_model'
-    _description = 'Test Model'
-
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
