63dc62d05df0cc3e303ca7beac6450addf2cf193
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-03 13:35:11 +0200

    pos missing

--- odoo3/pharmacy/__manifest__.py
+++ odoo3/pharmacy/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Pharmacy Management',
-    'version': '19.0.1.0.0',
+    'version': '17.0.6.0.0',
     'category': 'Pharmacy',
     'sequence': 1,
     'summary': 'Advanced Pharmacy Management with Box/Envelope Inventory',
