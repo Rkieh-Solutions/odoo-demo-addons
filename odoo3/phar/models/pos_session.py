ec67925d6b9bc00975b2ee59341fe0212b57d8ef
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-04 12:36:55 +0200

    ADD

--- odoo3/phar/models/pos_session.py
+++ odoo3/phar/models/pos_session.py
@@ -1,47 +1,5 @@
-from odoo import api, models
-
+from odoo import models
 
 class PosSession(models.Model):
     _inherit = 'pos.session'
-
-    def _loader_params_product_product(self):
-        result = super()._loader_params_product_product()
-        # Add all technical pharmacy fields required by the JS frontend
-        extra_fields = [
-            'is_box_product', 
-            'envelopes_per_box', 
-            'envelope_price',
-            'envelope_qty', 
-            'box_qty', 
-            'envelope_child_id', 
-            'parent_box_id',
-            'composition', 
-            'composition_text',
-            'code'
-        ]
-        for field in extra_fields:
-            if field not in result['search_params']['fields']:
-                result['search_params']['fields'].append(field)
-        return result
-
-    def _loader_params_product_template(self):
-        result = super()._loader_params_product_template()
-        extra_fields = [
-            'is_box_product', 
-            'envelopes_per_box', 
-            'envelope_price',
-            'envelope_qty', 
-            'box_qty', 
-            'envelope_child_id', 
-            'parent_box_id',
-            'composition', 
-            'composition_text',
-            'code'
-        ]
-        if 'search_params' not in result:
-            result['search_params'] = {'fields': []}
-        
-        for field in extra_fields:
-            if field not in result['search_params']['fields']:
-                result['search_params']['fields'].append(field)
-        return result
+    # No overrides needed for Odoo 20 as product loader mixins handle field synchronization.
