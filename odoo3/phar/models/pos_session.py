46a27fb1dd64110b6f9dc0ba2668d6b02b635e1a
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-04 12:12:06 +0200

    add

--- odoo3/phar/models/pos_session.py
+++ odoo3/phar/models/pos_session.py
@@ -23,3 +23,25 @@ class PosSession(models.Model):
             if field not in result['search_params']['fields']:
                 result['search_params']['fields'].append(field)
         return result
+
+    def _loader_params_product_template(self):
+        result = super()._loader_params_product_template()
+        extra_fields = [
+            'is_box_product', 
+            'envelopes_per_box', 
+            'envelope_price',
+            'envelope_qty', 
+            'box_qty', 
+            'envelope_child_id', 
+            'parent_box_id',
+            'composition', 
+            'composition_text',
+            'code'
+        ]
+        if 'search_params' not in result:
+            result['search_params'] = {'fields': []}
+        
+        for field in extra_fields:
+            if field not in result['search_params']['fields']:
+                result['search_params']['fields'].append(field)
+        return result
--- odoo3/phar/models/product_template.py
+++ odoo3/phar/models/product_template.py
@@ -460,6 +460,36 @@ class ProductTemplate(models.Model):
 
         return super().create(vals_list)
 
+    @api.model
+    def _load_pos_data_fields(self, config_id):
+        fields = super()._load_pos_data_fields(config_id)
+        # Technical pharmacy fields required by POS JS
+        extra = [
+            'is_box_product', 
+            'envelopes_per_box', 
+            'envelope_price',
+            'envelope_qty', 
+            'box_qty', 
+            'envelope_child_id', 
+            'parent_box_id',
+            'composition', 
+            'composition_text',
+            'code',
+            'qty_available'
+        ]
+        for f in extra:
+            if f not in fields:
+                fields.append(f)
+        return fields
+
+    @api.model
+    def _load_pos_data_read(self, records, config):
+        res = super()._load_pos_data_read(records, config)
+        product_map = {p.id: p.qty_available for p in records}
+        for r in res:
+            r['qty_available'] = product_map.get(r['id'], 0.0)
+        return res
+
 
     def write(self, vals):
         # Handle Margin/Price updates
