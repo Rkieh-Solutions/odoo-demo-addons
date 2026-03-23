0a8f4f9e3ea7c51c4a8b078a71f9eacaa585442f
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-23 11:10:38 +0200

    edit

--- odoo1/POS ALERT/pos_stock_alert/models/product_template.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    x_qty_to_warn = fields.Float(string='Quantity to Warn', default=0.0, help='Alert in POS when stock is below or equal to this quantity.')
