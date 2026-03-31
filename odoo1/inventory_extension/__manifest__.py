d65038ceccb30f3667d7decab5e1b792f3131e80
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-31 13:00:16 +0300

    edit

--- odoo1/inventory_extension/__manifest__.py
+++ odoo1/inventory_extension/__manifest__.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Inventory Extension',
+    'name': 'Inventory Enhancement',
     'version': '19.0.1.0.0',
+    'summary': 'Enhanced product image display, Margin and Margin % in Inventory',
+    'description': """
+        This module enhances the inventory experience by:
+        1. Providing a larger product image display with zoom/preview.
+        2. Adding Margin and Margin % fields to the product template for better performance tracking.
+    """,
     'category': 'Industry',
-    'author': 'Rkiehsoloutions',
     'sequence': 1,
-    'depends': ['base', 'product', 'stock'],
+    'author': 'Rkiehsoloutions',
+    'depends': ['base', 'product', 'stock', 'point_of_sale', 'product_expiry', 'sale', 'purchase'],
     'data': [
         'views/product_views.xml',
     ],
     'assets': {
         'web.assets_backend': [
-            'inventory_extension/static/src/css/product_style.css',
+            'inventory_enhancement/static/src/css/product_style.css',
         ],
     },
     'installable': True,
     'application': True,
     'auto_install': False,
     'license': 'LGPL-3',
-    'web_icon': 'inventory_extension,static/description/icon.png',
+    'web_icon': 'inventory_enhancement,static/description/icon.png',
 }
