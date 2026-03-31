c671e89aa18581383634f28c0509f0f64080e900
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-03-31 12:00:49 +0300

    edit

--- odoo4/inventory_enhancements/__manifest__.py
+++ odoo4/inventory_enhancements/__manifest__.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 {
     'name': 'Inventory Enhancements',
     'version': '16.0.1.0.0',
@@ -8,7 +8,7 @@
         1. Providing a larger product image display with zoom/preview.
         2. Adding Margin and Margin % fields to the product template for better performance tracking.
     """,
-    'category': 'Inventory/Inventory',
+    'category': 'Industry',
     'author': 'Antigravity',
     'sequence': 1,
     'depends': ['base', 'product', 'stock'],
@@ -24,4 +24,5 @@
     'application': True,
     'auto_install': False,
     'license': 'LGPL-3',
+    'web_icon': 'inventory_enhancements,static/description/icon.png',
 }
