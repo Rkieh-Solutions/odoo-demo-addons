1e2cebe9b1fc491d34146a7d2eb6e827bd5183a6
Omar Farhat <omar.farhat@rkiehsolutions.com>
2026-04-04 11:17:50 +0300

    edit

--- odoo2/warehouse_access_control/__manifest__.py
+++ odoo2/warehouse_access_control/__manifest__.py
@@ -11,8 +11,8 @@
         'purchase',
         'sale_management',
         'mrp',
-        'quality_control',
-        'maintenance',
+        # 'quality_control',
+        # 'maintenance',
         'account',
     ],
     'data': [
@@ -31,14 +31,14 @@
         'views/picking_route_map_views.xml',
         'views/sale_purchase_map_views.xml',
         'views/order_line_tier_views.xml',
-        'views/quality_capa_views.xml',
+        # 'views/quality_capa_views.xml',
         'views/dashboards/warehouse_dashboard_views.xml',
         'views/mobile_views.xml',
         'views/res_config_settings_views.xml',
         # Report
         'report/delivery_note_report.xml',
         'report/cargo_instructions_report.xml',
-        'report/quality_check_report.xml',
+        # 'report/quality_check_report.xml',
         # Menus - last, after all actions are defined
         'views/menus.xml',
     ],
